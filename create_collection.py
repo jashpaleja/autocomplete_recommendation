88bc95b10e285405a8f932a3e2aef73c119e9d5d
jashpaleja <jash.paleja@somaiya.edu>
2023-07-24 12:46:14 -0400

    whoosh api

--- create_collection.py
+++ create_collection.py
@@ -1,9 +1,14 @@
+# from whoosh.analysis import NgramWordAnalyzer, StemmingAnalyzer, StandardAnalyzer
+from whoosh.index import open_dir,create_in
+from whoosh.fields import *
+from collections import OrderedDict
+import os
+import shutil
+import re
 from tqdm import tqdm
 import ujson
 from glob import glob
 import os
-from collections import OrderedDict
-import re
 
 def create_and_save_inv_data(input_file_path: str, type_of_data: str ,get_all_lists: list, get_all_strings: list, weight: str):
     oldData = open(input_file_path, 'r')
@@ -21,14 +26,13 @@ def create_and_save_inv_data(input_file_path: str, type_of_data: str ,get_all_li
             includesArr = list(set(includesArr))
             for strings in includesArr:
                 valSet = inv_data.setdefault(strings,{})
-                valSet[key] = value.get(weight,0)
+                valSet[key] = value.get(weight,float(0.0))
 
     document_array = []
     for index,(i, j) in enumerate(inv_data.items()):
         icd_code = max(inv_data[i], key=inv_data[i].get)
         document_array.append(
         {
-            'id': str(index + 1),
             'string_search': i,
             'code': icd_code,
             'norm_count': inv_data[i][icd_code],
@@ -37,8 +41,33 @@ def create_and_save_inv_data(input_file_path: str, type_of_data: str ,get_all_li
         )
     return document_array
 
+def indexing_data(inverse_results):
+    # Cleaning preindexed data
+    for type_of_data, values in inverse_results.items():
+        try:
+            shutil.rmtree(f"indexed_data/{type_of_data}_index")
+            print(f"Folder 'indexed_data/{type_of_data}_index' deleted successfully.")
+        except OSError as e:
+            pass
+            # print(f"Error: {e}")
 
-def create_collection_from_tab(folder_path: str, client):
+    # Indexing data
+    for type_of_data, values in inverse_results.items():
+        schema = Schema(name=STORED, string_search=TEXT(stored=True), code=STORED, norm_count=STORED)
+        if not os.path.exists(f"indexed_data"):
+            os.mkdir(f"indexed_data")
+        if not os.path.exists(f"indexed_data/{type_of_data}"):
+            os.mkdir(f"indexed_data/{type_of_data}_index")
+        index = create_in(f"indexed_data/{type_of_data}_index", schema)
+        writer = index.writer()
+        print(f'Creating index for {type_of_data} data')
+        for obj in tqdm(values):
+            if obj['name']:
+                writer.add_document(name=obj['name'], string_search=u''+obj['string_search'],
+                            code=obj['code'], norm_count=float(obj['norm_count']), _boost=float(obj['norm_count']))
+        writer.commit()
+
+def create_indexed_data(folder_path:str):
     json_to_combine = {
         'icd':{
             'key': 'ICD',
@@ -64,7 +93,7 @@ def create_collection_from_tab(folder_path: str, client):
     for f in glob(os.path.join(folder_path, '*.json')):
         file_name = os.path.basename(f)
         code_type = file_name.split("_")[0].lower()
-        print(f"Indexing {code_type} data from file:- {file_name}")
+        print(f"Preprocessing {file_name}")
         v = json_to_combine[code_type]
         inverse_results[code_type] = create_and_save_inv_data(
             folder_path+file_name, 
@@ -73,115 +102,21 @@ def create_collection_from_tab(folder_path: str, client):
             v['get_all_strings'], 
             v['weight'])
         
-    for _ in list(inverse_results.keys()):
-        try:
-            client.collections[_].delete()
-        except Exception as e:
-            print(f"Collection {_} doesn't exist", e)
-            # pass
-
-        schema = {
-        'name': _,
-        'fields': [
-            {
-            'name'  :  'string_search',
-            'type'  :  'string',
-            "index": True
-            },
-            {
-            'name'  :  'code',
-            'type'  :  'string',
-            'facet': True,
-            },
-            {
-            'name'  :  'norm_count',
-            'type'  :  'float',
-            'facet': True,
-            },
-            {
-            'name'  :  'name',
-            'type'  :  'string',
-            'facet': True,
-            }
-        ],
-        'default_sorting_field': 'norm_count'
-        }
-        print(f'Indexing {_} data')
-        client.collections.create(schema)
-        data = inverse_results[_]
-        client.collections[_].documents.import_(data, {'action': 'create'})
-
-
+    indexing_data(inverse_results)
 
 def clean_string(string):
     cleaned_string = re.sub(r"[^\w\s]", "", string)
-    return cleaned_string.strip()
+    return re.sub(' +', ' ', cleaned_string)
 
-def all_responses_combined(client, search_type, search_params, max_pages, first_response):
-    response = first_response['grouped_hits']
-    i = 2
-    while i<=max_pages: #max_pages
-        page1_response = client.collections[search_type].documents.search(search_params)
-        response.extend(page1_response['grouped_hits'])
-        i+=1
-    search_params['page']=i
-    return response
-
-
-def match_regex_contains(query, string, ordered, query_length):
-    if query_length>1:
-        if ordered: # it will check if the the query is present in the sentence in ordered format else check if unordered format
-            pattern = r'\b.*?' + r'.*?'.join(re.escape(word) for word in query.split()) + r'.*?\b'
-            return bool(re.search(pattern, string, flags=re.IGNORECASE))
-        words = set(query.lower().split())
-        return all(word in string.lower() for word in words)
-    else:
-        return bool(re.search(r'^'+query, string, flags=re.IGNORECASE)) if ordered else bool(re.search(r''+query, string, flags=re.IGNORECASE)) # it will check if the the sentence start with the query else it will check if the sentence contains the query
-
-
-def filtered_sort_data(query_string , response):
-    priority1 = [] # store if any string from the code has a exact match
-    priority2 = [] # store if any string from the code has a all the words present in the string from the query and the order of the words remain the same
-    priority3 = [] # store if any string from the code has a all the words present in the string from the query and are not in the same order
-    priority4 = [] # store string from the code even if the word has 1 word present in the string and also store fuzzy matches.
-    for groups in response:
-        data_to_append={
-            'code':groups['group_key'][0],
-            'name':groups['group_key'][1],
-            'norm_count':groups['group_key'][2],
-        }
-        response = []
-        exact = []
-        all_contains_ordered=[]
-        all_contains_unordered=[]
-        others = []
-        query_length = len(query_string.split())
-        score_obj = {'1':0, '2':0, '3':0, '4':0}
-        for document in groups['hits']:
-            matched_string = document['document']['string_search']
-            if query_string.lower() == matched_string.lower():
-                exact.append(matched_string)
-                score_obj['1']+=1
-            else:
-                if match_regex_contains(query_string, matched_string, False, query_length):
-                    if match_regex_contains(query_string, matched_string, True, query_length):
-                        all_contains_ordered.append(matched_string)
-                        score_obj['2']+=1
-                    else:    
-                        all_contains_unordered.append(matched_string)
-                        score_obj['3']+=1
-                else:
-                    others.append(matched_string)
-                    score_obj['4']+=1
-        
-        data_to_append['matched_strings'] = exact+all_contains_ordered+all_contains_unordered+others
-        if score_obj['1']>0:
-            priority1.append(data_to_append)
-        elif score_obj['2']>0:
-            priority2.append(data_to_append)
-        elif score_obj['3']>0:
-            priority3.append(data_to_append)
-        else:
-            priority4.append(data_to_append)
-
-    return priority1+priority2+priority3+priority4
+def filteration(results):
+    top_results=OrderedDict()
+    for result in results:
+        if result['code'] not in top_results:
+            top_results[result['code']] = {
+                'code': result['code'],
+                'name': result['name'],
+                'norm_count': result['norm_count'],
+                'string_search': [],
+            }
+        top_results[result['code']]['string_search'].append(result['string_search'])
+    return list(top_results.values())
--- predictor.py
+++ predictor.py
@@ -1,55 +1,66 @@
-import typesense
-from flask import Flask, request, abort
-from create_collection import create_collection_from_tab, clean_string, all_responses_combined, filtered_sort_data
+# from whoosh.analysis import NgramWordAnalyzer, StemmingAnalyzer, StandardAnalyzer
+from whoosh.qparser import QueryParser,FuzzyTermPlugin
+from whoosh.index import open_dir
+from whoosh import columns, fields, index, sorting
+from whoosh.fields import *
 import time
-import math
-
-client = typesense.Client({
-    'nodes': [{
-        'host': 'localhost',
-        'port': '8081',
-        'protocol': 'http'
-    }],
-    'api_key': 'abcd'
-})
+from create_collection import filteration,clean_string,create_indexed_data
+from flask import Flask, request, jsonify, abort
+from collections import OrderedDict
 
 folder_path = './tab_data/'
-create_collection_from_tab(folder_path, client)
+y_n = input('Do you want to index the data. Enter y or n:')
+if y_n.lower()=='y':
+    create_indexed_data(folder_path)
+index_data = {
+    'icd': open_dir(f"indexed_data/icd_index"),
+    'cpt': open_dir(f"indexed_data/cpt_index"),
+    'med': open_dir(f"indexed_data/med_index")
+}
+
 
 app = Flask(__name__)
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('q')
     query_string = clean_string(query)
-    search_type = request.args.get('type')
-    exhaustive_search = request.args.get('exhaustive_search')
-    matched_string_all = request.args.get('matched_string_all')
+    type_of_data = request.args.get('type')
     if query_string:
-        search_params = {
-            'q': query_string,
-            'query_by': 'string_search',
-            'sort_by': 'norm_count:desc',
-            "exhaustive_search": bool(exhaustive_search=='true' and len(query_string)>1),
-            "per_page": 250,
-            'page':1,
-            'group_by': 'code,name,norm_count',
-            'group_limit': 20,
-        }
-        if bool(matched_string_all=='true'):
-            search_params['group_limit'] = 50
+        wildcard = '* '.join(query_string.split())+'*'
+        fuzzy = '~2 '.join(query_string.split())+'~2'
         start_time = time.time()
-        first_response = client.collections[search_type].documents.search(search_params)
-        all_page_response = all_responses_combined(client,search_type,search_params, math.ceil(first_response['found']/250), first_response)
-        typesense_time = time.time()
-        filtered_arr = filtered_sort_data(query_string, all_page_response)
+        index = index_data[type_of_data]
+        parser = QueryParser("string_search", schema=index.schema)
+        parser.add_plugin(FuzzyTermPlugin())
+        query = parser.parse(wildcard)
+        type_of_search = 'wildcard'
+        with index.searcher() as searcher:
+            results = searcher.search(query, limit=80)
+            if results.is_empty():
+                query = parser.parse(fuzzy)
+                results = searcher.search(query, limit=80)
+                type_of_search = 'fuzzy'
+            
+            # merging the strings with same code
+            top_results=OrderedDict()
+            for result in results:
+                if result['code'] not in top_results:
+                    top_results[result['code']] = {
+                        'code': result['code'],
+                        'name': result['name'],
+                        'norm_count': result['norm_count'],
+                        'string_search': [],
+                    }
+                top_results[result['code']]['string_search'].append(result['string_search'])
+
+        list_of_values = list(top_results.values())
         end_time = time.time()
-        return {
+                
+        return {   
             'search_time_ms': int((end_time - start_time)*1000),
-            'found': len(all_page_response),
-            'hits': filtered_arr,
-            'typesense_search_time' : int((typesense_time - start_time)*1000),
-            'post_processing_time':int((end_time - typesense_time)*1000),
-            'exhaustive_search':search_params['exhaustive_search'],
+            'found': len(list_of_values),
+            'type_of_search': type_of_search + ' (This field is added just for testing purposes)',
+            'hits': list_of_values,
         }
     else:
         return abort(403, "String Cannot be Empty")
